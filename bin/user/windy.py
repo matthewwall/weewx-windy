0637e9cf5091c893af14f8d52ea692d2a7d6879a
Tom Keffer <tkeffer@gmail.com>
2020-05-08 11:54:21 -0700

    Ported to WeeWX V4 logging. Simplified a bit.

--- bin/user/windy.py
+++ bin/user/windy.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Matthew Wall
+# Copyright 2019-2020 Matthew Wall
 
 """
 This is a weewx extension that uploads data to a windy.com
@@ -28,17 +28,20 @@ The default station identifier is 0.
 
 # deal with differences between python 2 and python 3
 try:
-    # noinspection PyCompatibility
-    from Queue import Queue
+    # Python 3
+    import queue
 except ImportError:
-    # noinspection PyCompatibility
-    from queue import Queue
+    # Python 2
+    # noinspection PyUnresolvedReferences
+    import Queue as queue
 
 try:
-    from urllib import urlencode
-except ImportError:
-    # noinspection PyCompatibility
+    # Python 3
     from urllib.parse import urlencode
+except ImportError:
+    # Python 2
+    # noinspection PyUnresolvedReferences
+    from urllib import urlencode
 
 from distutils.version import StrictVersion
 import json
@@ -49,9 +52,9 @@ import weewx
 import weewx.manager
 import weewx.restx
 import weewx.units
-from weeutil.weeutil import to_bool
+from weeutil.weeutil import to_bool, to_int
 
-VERSION = "0.6"
+VERSION = "0.7"
 
 REQUIRED_WEEWX = "3.8.0"
 if StrictVersion(weewx.__version__) < StrictVersion(REQUIRED_WEEWX):
@@ -59,22 +62,32 @@ if StrictVersion(weewx.__version__) < StrictVersion(REQUIRED_WEEWX):
                                    % (REQUIRED_WEEWX, weewx.__version__))
 
 try:
-    from weeutil.log import logdbg, loginf, logerr
+    # Test for new-style weewx logging by trying to import weeutil.logger
+    import weeutil.logger
+    import logging
+    log = logging.getLogger(__name__)
+
+    def logdbg(msg):
+        log.debug(msg)
+
+    def loginf(msg):
+        log.info(msg)
+
+    def logerr(msg):
+        log.error(msg)
+
 except ImportError:
+    # Old-style weewx logging
     import syslog
 
-
     def logmsg(level, msg):
-        syslog.syslog(level, 'restx: Windy: %s' % msg)
-
+        syslog.syslog(level, 'windy: %s' % msg)
 
     def logdbg(msg):
         logmsg(syslog.LOG_DEBUG, msg)
 
-
     def loginf(msg):
         logmsg(syslog.LOG_INFO, msg)
-
 
     def logerr(msg):
         logmsg(syslog.LOG_ERR, msg)
@@ -89,22 +102,17 @@ class Windy(weewx.restx.StdRESTbase):
         site_dict = weewx.restx.get_site_dict(cfg_dict, 'Windy', 'api_key')
         if site_dict is None:
             return
-        site_dict.setdefault('station', 0)
-        site_dict.setdefault('server_url', Windy.DEFAULT_URL)
 
-        # FIXME: we should not have to do this here!
-        binding = site_dict.pop('binding', 'wx_binding')
-        mgr_dict = weewx.manager.get_manager_dict_from_config(
-            cfg_dict, binding)
+        try:
+            site_dict['manager_dict'] = weewx.manager.get_manager_dict_from_config(cfg_dict, 'wx_binding')
+        except weewx.UnknownBinding:
+            pass
 
-        self.archive_queue = Queue()
-        self.archive_thread = WindyThread(self.archive_queue,
-                                          manager_dict=mgr_dict,
-                                          **site_dict)
+        self.archive_queue = queue.Queue()
+        self.archive_thread = WindyThread(self.archive_queue, **site_dict)
 
         self.archive_thread.start()
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
-        loginf("Data will be uploaded to %s" % site_dict['server_url'])
 
     def new_archive_record(self, event):
         self.archive_queue.put(event.record)
@@ -112,7 +120,7 @@ class Windy(weewx.restx.StdRESTbase):
 
 class WindyThread(weewx.restx.RESTThread):
 
-    def __init__(self, q, api_key, station, server_url=Windy.DEFAULT_URL,
+    def __init__(self, q, api_key, station=0, server_url=Windy.DEFAULT_URL,
                  skip_upload=False, manager_dict=None,
                  post_interval=None, max_backlog=sys.maxsize, stale=None,
                  log_success=True, log_failure=True,
@@ -129,8 +137,9 @@ class WindyThread(weewx.restx.RESTThread):
                                           timeout=timeout,
                                           retry_wait=retry_wait)
         self.api_key = api_key
-        self.station = station
+        self.station = to_int(station)
         self.server_url = server_url
+        loginf("Data will be uploaded to %s" % self.server_url)
         self.skip_upload = to_bool(skip_upload)
 
     def format_url(self, _):
@@ -183,17 +192,19 @@ class WindyThread(weewx.restx.RESTThread):
 #   PYTHONPATH=bin python bin/user/windy.py
 
 if __name__ == "__main__":
-    class FakeMgr(object):
-        table_name = 'fake'
-
-        # noinspection PyUnusedLocal,PyMethodMayBeStatic
-        def getSql(self, query, value):
-            return None
-
-
     weewx.debug = 2
-    queue = Queue()
-    t = WindyThread(queue, api_key='123', station=0)
+
+    try:
+        # WeeWX V4 logging
+        weeutil.logger.setup('windy', {})
+    except NameError:
+        # WeeWX V3 logging
+        syslog.openlog('windy', syslog.LOG_PID | syslog.LOG_CONS)
+        syslog.setlogmask(syslog.LOG_UPTO(syslog.LOG_DEBUG))
+
+    q = queue.Queue()
+    t = WindyThread(q, api_key='123', station=0)
+    t.start()
     r = {'dateTime': int(time.time() + 0.5),
          'usUnits': weewx.US,
          'outTemp': 32.5,
@@ -202,5 +213,6 @@ if __name__ == "__main__":
          'windSpeed': 10,
          'windDir': 32}
     print(t.format_url(r))
-    #    print(t.get_post_body(r))
-    t.process_record(r, FakeMgr())
+    q.put(r)
+    q.put(None)
+    t.join(30)
